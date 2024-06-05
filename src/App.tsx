08926a8a6073ff4c8e2b435aafd22fd9d7559401
cankecilioglu <cankecilioglu@gmail.com>
2024-06-05 21:13:04 +0300

    states are added and jsx basics done

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,39 @@
+import { useState } from "react";
 import "./style.css";
-export default function App() {}
+
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+          <button className="btn">Add</button>
+        </div>
+      </form>
+      <h1 className="header">To-Do-List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+}
